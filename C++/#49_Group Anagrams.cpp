8abb31f03571b18b3740751a966ff0c846db1b27
OjasWadhwani <ojaswadhwani098@gmail.com>
2024-03-20 21:58:33 +0530

    49 - 2

--- C++/#49_Group Anagrams.cpp
+++ C++/#49_Group Anagrams.cpp
@@ -12,3 +12,38 @@ public:
         return result;
     }
 };
+
+/**
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string, vector<string>> mp;
+
+        for(auto i: strs) {
+            string sortedString = i;
+            sort(sortedString.begin(), sortedString.end());
+
+            // cout<<sortedString<<endl;
+
+            mp[sortedString].push_back(i);
+        }
+
+        // for(auto i: mp) {
+        //     for (auto j: i.second) {
+        //         cout<<j<<endl;
+        //     }
+        //     cout<<endl;
+        // }
+
+        vector<vector<string>> answer{mp.size()};
+        
+        int answerIndex = 0;
+        for (auto i: mp) {
+            answer[answerIndex] = i.second;
+            answerIndex++;
+        }
+
+        return answer;
+    }
+};
+*/
